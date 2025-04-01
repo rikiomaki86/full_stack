1e845f27dda96a6b3597b5dcfd5fd475a17deef1
rikiomaki86 <rikiomaki86@gmail.com>
2025-04-01 16:33:59 -0300

    Mario Rikio Makimoto

---
+++ jogo/script.js
@@ -0,0 +1,25 @@
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+function checkGuess() {
+    let userGuess = document.getElementById("guessInput").value;
+    let message = document.getElementById("message");
+
+    
+    if (userGuess === "" || isNaN(userGuess)) {
+        message.textContent = "Por favor, insira um número válido.";
+        return;
+    }
+
+    userGuess = parseInt(userGuess);
+
+    // Lógica de comparação
+    if (userGuess < randomNumber) {
+        message.textContent = "O número é maior! Tente novamente.";
+        document.body.style.backgroundColor = "red"; 
+    } else if (userGuess > randomNumber) {
+        message.textContent = "O número é menor! Tente novamente.";
+        document.body.style.backgroundColor = "red"; 
+    } else {
+        message.textContent = "Parabéns! Você acertou o número.";
+        document.body.style.backgroundColor = "green"; 
+}
